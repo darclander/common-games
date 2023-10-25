55788d3987ad8af6a414a9d9639f958fc9e6e3ed
darclander <lukedarclander@gmail.com>
2023-10-25 13:54:40 +0200

    Initial commit for score class

---
+++ snake/src/headers/Score.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+class Score {
+    public:
+        Score();
+        ~Score();
+
+    private:
+        m_textureScore;
+
+};
