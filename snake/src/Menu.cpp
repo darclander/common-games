71b30e4e21d2b964eaa4f70bbf4831d4692e9910
darclander <lukedarclander@gmail.com>
2023-12-10 16:55:32 +0100

    Updated menu
    Options are now available although a bit buggy still

--- snake/src/Menu.cpp
+++ snake/src/Menu.cpp
@@ -7,7 +7,8 @@ Menu::Menu(SDL_Renderer *renderer, int xPos, int yPos, int width, int height, TT
     m_yPos      = yPos;
     m_width     = width;
     m_height    = height;
-
+    m_limit     = 0;
+    m_menuIndex = 0; // Starting at first value of a menu.
 
     m_font = font;
     std::cout << &m_items << std::endl;
@@ -15,8 +16,8 @@ Menu::Menu(SDL_Renderer *renderer, int xPos, int yPos, int width, int height, TT
 
 }
 
-Text Menu::createText(const std::string &name, int xPos, int yPos) {
-    SDL_Color textColor = {255, 255, 255, 255}; // White color
+Text Menu::createText(const std::string &name, int xPos, int yPos, SDL_Color textColor) {
+    
     SDL_Surface *textSurface = TTF_RenderText_Solid(m_font, name.c_str(), textColor);
 
     if (!textSurface) {
@@ -35,24 +36,66 @@ Text Menu::createText(const std::string &name, int xPos, int yPos) {
     textInfo.texture    = textTexture;
     textInfo.xPos       = xPos;
     textInfo.yPos       = yPos;
+    textInfo.name       = name;
 
     SDL_FreeSurface(textSurface);
     return textInfo;
 }
 
-template int Menu::addItem(const std::string &name, int &reference_value);
-template int Menu::addItem(const std::string &name, float &reference_value);
+bool Menu::updateText(Text &txt, SDL_Color textColor) {
+    SDL_Surface *textSurface = TTF_RenderText_Solid(m_font, txt.name.c_str(), textColor);
+    if (!textSurface) {
+        std::cerr << "Unable to render text surface! SDL_ttf Error: " << TTF_GetError() << std::endl;
+    }
+
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(m_renderer, textSurface);
+    if (!textTexture) {
+        std::cerr << "Unable to create texture from rendered text! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_FreeSurface(textSurface);
+    }
+    SDL_FreeSurface(textSurface);
+
+    txt.texture = textTexture;
+    return true;
+}
+
+template int Menu::addItem(const std::string &name, int type, int   &reference_value);
+template int Menu::addItem(const std::string &name, int type, float &reference_value);
 
 template <typename T>
-int Menu::addItem(const std::string &name, T &reference_value) {
+int Menu::addItem(const std::string &name, int type, T &reference_value) {
 
-    Text textInfo = createText(name, m_xPos, m_yPos + m_items.size() * 50);
+    Text textInfo = createText(name, m_xPos, m_yPos + m_items.size() * 50, menuc::WHITE);
     MenuItem mi;
     mi.menuText = textInfo;
 
     m_items.push_back(mi);
 
     return m_items.size(); 
+}
+
+int Menu::update(double deltaTime) {
+    m_limit += deltaTime;  
+    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+
+    if(m_limit > 100.f) {
+        m_limit = 0;
+        if(key_state[SDL_SCANCODE_DOWN]) {
+            updateText(m_items[m_menuIndex].menuText, menuc::WHITE);
+            if(m_menuIndex < m_items.size()-1) m_menuIndex++;
+            updateText(m_items[m_menuIndex].menuText, menuc::RED);
+        } else if (key_state[SDL_SCANCODE_UP]) {
+            updateText(m_items[m_menuIndex].menuText, menuc::WHITE);
+            if(m_menuIndex > 0) m_menuIndex--;
+            updateText(m_items[m_menuIndex].menuText, menuc::RED);
+        }
+    }
+
+    if(key_state[SDL_SCANCODE_RETURN]) {
+        return m_menuIndex + 1;
+    }
+
+    return 0;
 }
 
 void Menu::render() {
--- snake/src/headers/Menu.hpp
+++ snake/src/headers/Menu.hpp
@@ -9,11 +9,19 @@
 #define MENU_OPTION     0x0
 #define MENU_BAR        0x1
 
+namespace menuc {
+    const SDL_Color RED     = {255, 0,   0,   255};
+    const SDL_Color GREEN   = {0,   255, 0,   255};
+    const SDL_Color BLUE    = {0,   0,   255, 255};
+    const SDL_Color WHITE   = {255, 255, 255, 255};
+}
+
 struct Text {
     int width;
     int height;
     int xPos;
     int yPos;
+    std::string name;
     SDL_Texture *texture;
 };
 
@@ -32,9 +40,10 @@ class Menu {
 
 
         template <typename T>
-        int addItem(const std::string &name, T &referenceValue);
+        int addItem(const std::string &name, int type, T &referenceValue);
 
         void render();
+        int update(double deltaTime); 
 
     private:
 
@@ -42,8 +51,11 @@ class Menu {
         int m_height;
         int m_xPos;
         int m_yPos;
+        int m_menuIndex;
+        int m_limit;
 
-        Text createText(const std::string &name, int xPos, int yPos);
+        Text createText(const std::string &name, int xPos, int yPos, SDL_Color textColor);
+        bool updateText(Text &t, SDL_Color textColor);
         std::vector<MenuItem> m_items;
 
         TTF_Font *m_font;
