f14784b3777b9a462647e524bb7304718ec410c4
Johannes <44120608+Kumole@users.noreply.github.com>
2024-02-02 01:23:58 +0200

    added testing file

---
+++ server/ServerTester.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerTester {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 12345);
+
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    InputStream inputStream = socket.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        String message = new String(buffer, 0, bytesRead);
+                        System.out.println("Server: " + message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            Thread sendThread = new Thread(() -> {
+                try {
+                    OutputStream outputStream = socket.getOutputStream();
+                    Scanner scanner = new Scanner(System.in);
+                    String message;
+                    while (true) {
+                        message = scanner.nextLine();
+                        outputStream.write((message).getBytes());
+                        outputStream.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            receiveThread.start();
+            sendThread.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
